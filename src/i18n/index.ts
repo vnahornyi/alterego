f9306d404d1b601da5e30731f3d12338b12c81f4
Vladyslav Nahornyi <vnahornyi@outlook.com>
2023-02-19 15:10:27 +0200

    fix lang bug

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 
 import resources from "./resources";
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: ["en", "uk"],
-    resources,
-  });
+i18n.use(initReactI18next).init({
+  lng: "en",
+  fallbackLng: ["en", "uk"],
+  resources,
+});
